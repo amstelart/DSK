1bbf070c44aafecd63e9c9885cdfa73dd5693ad9
Артем <amstelart@MacBook-Pro-Artem.local>
2017-12-27 07:43:15 +0300

    fix feedback form

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -32,9 +32,18 @@ $( document ).ready(function() {
 });
 
 // Изоляция без jQuery
-// (function(){
-//   // code
-// }());
+$(function(){
+  $('input[type="time"][value="now"]').each(function(){
+    var d = new Date(),
+        h = d.getHours(),
+        m = d.getMinutes();
+    if(h < 10) h = '0' + h;
+    if(m < 10) m = '0' + m;
+    $(this).attr({
+      'value': h + ':' + m
+    });
+  });
+});
 
 // На проекте нет jQuery, но хочется $( document ).ready...
 // function ready(fn) {
